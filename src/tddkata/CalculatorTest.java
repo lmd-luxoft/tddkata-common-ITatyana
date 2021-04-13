9412a851b40408d1852418c878472aa1df6b1609
Администратор <IvanovaTG@ugard.cbr.ru>
2021-04-13 13:59:08 +0300

    IvanovaTG

---
+++ src/tddkata/CalculatorTest.java
@@ -0,0 +1,52 @@
+package tddkata;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class CalculatorTest {
+
+	@Test
+	public void Test() {
+		Calc calc = new Calc();
+		int actual = calc.sum("");
+			int expected =0;
+
+			assertEquals(expected,actual);
+	}
+	@Test
+	public void Test1() {
+		Calc calc = new Calc();
+		int actual = calc.sum("1");
+		int expected =0;
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void Test2() {
+		Calc calc = new Calc();
+		int actual = calc.sum(";");
+		int expected =0;
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void Test3() {
+		Calc calc = new Calc();
+		int actual = calc.sum(null);
+		int expected =0;
+
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void Test4() {
+		Calc calc = new Calc();
+		int actual = calc.sum("abs");
+		int expected =0;
+
+		assertEquals(expected,actual);
+	}
+
+}
